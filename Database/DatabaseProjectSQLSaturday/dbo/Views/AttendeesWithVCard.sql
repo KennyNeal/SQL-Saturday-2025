94592f225cd7880147eaea168af9ab16e8ef74a0
Kenny Neal <kneal30@gmail.com>
2025-07-21 10:48:48 -0500

    fix: Enhance vCard generation to include Job Title and Twitter nickname for attendees

--- Database/DatabaseProjectSQLSaturday/dbo/Views/AttendeesWithVCard.sql
+++ Database/DatabaseProjectSQLSaturday/dbo/Views/AttendeesWithVCard.sql
@@ -45,13 +45,25 @@ AS
                                        a.Twitter_X_UserName
                                END, ''
                            ) + CHAR(13) + CHAR(10) +
-        'ORG:' + isnull(REPLACE(REPLACE(a.Company, ',', '\,'), '''', ''''),'') + ';' + CHAR(13) + CHAR(10) +
+        'ORG:' + isnull(REPLACE(REPLACE(a.Company, ',', '\,'), '''', ''''),'') + CHAR(13) + CHAR(10) +
         'TITLE:' + ISNULL(a.Job_Title,'') + CHAR(13) + CHAR(10) +
         'EMAIL;TYPE=WORK,INTERNET,pref:' + ISNULL(a.Email, '') + CHAR(13) + CHAR(10) +
         'ADR;TYPE=WORK:;;' + '' + ';' + CHAR(13) + CHAR(10) +
         'URL;TYPE=WORK:' + ISNULL(a.Website, '') + CHAR(13) + CHAR(10) +
         'URL:' + ISNULL(a.Blog, '') + CHAR(13) + CHAR(10) +
-        'NOTE;ENCODING=QUOTED-PRINTABLE:SQL Saturday Baton Rouge 2025 Contact' + CHAR(13) + CHAR(10) +
+        'NOTE;ENCODING=QUOTED-PRINTABLE:SQL Saturday Baton Rouge 2025 Contact' 
+        + CASE WHEN ISNULL(a.Job_Title, '') <> '' THEN ' | Job Title: ' + a.Job_Title ELSE '' END
+        + CASE WHEN ISNULL(a.Twitter_X_UserName, '') <> '' THEN ' | Nickname: ' + 
+            ISNULL(CASE
+                WHEN LEFT(a.Twitter_X_UserName, 1) <> '@' AND LEFT(a.Twitter_X_UserName, 2) <> '''@' THEN
+                    '@' + REPLACE(a.Twitter_X_UserName, 'https://twitter.com/', '')
+                WHEN LEFT(a.Twitter_X_UserName, 2) = '''@' THEN
+                    REPLACE(a.Twitter_X_UserName, '''@', '@')
+                ELSE
+                    a.Twitter_X_UserName
+            END, '')
+        ELSE '' END
+        + CHAR(13) + CHAR(10) +
         'END:VCARD' + CHAR(13) + CHAR(10) AS vCard
     FROM
         [dbo].[Attendees] AS a
